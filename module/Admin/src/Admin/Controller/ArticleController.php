9104fba3bb8959290fd49baae9b5d371ff827c08
Core <Core@DESKTOP-HNJOQ80>
2017-08-22 15:40:44 +0300

    Working on article edit-part

--- module/Admin/src/Admin/Controller/ArticleController.php
+++ module/Admin/src/Admin/Controller/ArticleController.php
@@ -6,6 +6,10 @@ use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
 use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
 use Zend\Paginator\Paginator;
 
+use Blog\Entity\Article;
+use Admin\Form\ArticleAddForm;
+use DcotrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+
 class ArticleController extends BaseController
 {
     public function indexAction(){
@@ -18,9 +22,52 @@ class ArticleController extends BaseController
         $adapter = new DoctrineAdapter(new ORMPaginator($query));
         
         $paginator = new Paginator($adapter);
-        $paginator->setDefaultItemCountPerPage(1);
+        $paginator->setDefaultItemCountPerPage(10);
         $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
 
         return array('articles' => $paginator);
+    }
+    
+    public function addAction (){
+        $em = $this->getEntityManager();
+        $form = new ArticleAddForm($em);
+        
+        
+        
+        $request = $this->getRequest();
+        if($request->isPost()){
+            $message = $status = getPost();
+            
+            $article = new Article();
+            $form->setHydrator(new DoctrineHydrator($em, '\Article'));
+            $form->bind($article);
+            $form->setData($data);
+            
+            if($form->isValid()){
+                $em->persist($article);
+                $em->flush();
+                
+                $status = 'success';
+                $message = 'Article was added';
+            } else {
+                $status = 'error';
+                $message = 'Article wasn\'t added';
+                
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors){
+                    foreach ($errors->getMessages() as $error){
+                        $message .= ' ' . $error;
+                    }
+                }
+            }
+        } else {
+            return array('form' => $form);
+        }
+        
+        if($message){
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                    ->addMessage($message);
+        }
+        return $this->redirect()->toRoute('admin/article');
     }
 }
