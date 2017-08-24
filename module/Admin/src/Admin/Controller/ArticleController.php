deddf862f4b4f03fe640b2ea1f7068fd8793405c
Core <Core@DESKTOP-HNJOQ80>
2017-08-24 14:27:21 +0300

    Article add-part ready. Working on edit part.

--- module/Admin/src/Admin/Controller/ArticleController.php
+++ module/Admin/src/Admin/Controller/ArticleController.php
@@ -73,7 +73,53 @@ class ArticleController extends BaseController
     
     public function editAction(){
         $em = $this->getEntityManager();
+        $status = $message = '';
         $form = new ArticleAddForm($em);
         
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $article = $em->find('Blog\Entity\Article', $id);
+
+        if(empty($article)){
+            $message = "Article not found";
+            $status = 'error';
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                        ->addMessage($message);
+            return $this->redirect()->toRoute('admin/article');
+        }
+        
+        $form->bind($article);
+        $request = $this->getRequest();
+        
+        if($request->isPost()){
+            $data = $request->getPost();
+            $form->setData($data);
+            
+            if($form->isValid()){
+                $em->persist($category);
+                $em->flush();
+                
+                $status = 'success';
+                $message = 'Category was updated';
+            } else {
+                $status = 'error';
+                $message = 'Category wasn\'t updated. Parameter\'s error!';
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors){
+                    foreach($errors->getMessages() as $error){
+                        $message .= ' ' . $error;
+                    }
+                }
+            } 
+        } else {
+            return array('form' => $form, 'id' => $id);
+        }
+        
+        if($message){
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                        ->addMessage($message);
+        }
+        
+        return $this->redirect()->toRoute('admin/article');
     }
 }
